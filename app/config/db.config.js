530adefabeee7b6a9cfd6b44d2c7ad69fa450aad
Bibin.bennny88 <bibin.benny88@outlook.com>
2023-03-30 10:53:52 +0530

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://citpocdb:qtToF9mZCFGNopKTNcjM1Cz6njwHk9X2ZOvHg3pr3crnsbgyrSNfcuu8HEvlRslxYJkyTDsRRs0RACDbLUTmGA==@citpocdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@citpocdb@"
+};
