e9a13aa1cc323381c05f2451f6ac3b391aa40649
Mithra M S <mithrams93@gmail.com>
2023-04-06 11:38:31 +0530

    updated cors option

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-//const path = require("path");
+const path = require("path");
 
 const app = express();
-//const static_path = path.join(__dirname, "./public" );
-//app.use(express.static(static_path));
+const static_path = path.join(__dirname, "./public" );
+app.use(express.static(static_path));
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://citengin.azurewebsites.net"
 };
 
 app.use(cors(corsOptions));
