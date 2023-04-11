c230cea7c1adacc33fd1f5ec321d21a3f408fbbc
Mithra M S <mithrams93@gmail.com>
2023-04-11 16:04:54 +0530

    updated server.js without metrics

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 //const path = require("path");
-const prometheus = require('prom-client');
-const { httpRequestCounter } = require('./metrics');
-const client = require('prom-client');
-const promBundle = require('express-prom-bundle');
 
 const app = express();
 //const static_path = path.join(__dirname, "./public" );
-
-const metricsMiddleware = promBundle({
-  includeMethod: true,
-  includePath: true,
-  promClient: {
-    collectDefaultMetrics: {},
-    register: new client.Registry(),
-  },
-});
-
-
 //app.use(express.static(static_path));
 
 var corsOptions = {
@@ -32,7 +17,6 @@ app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-app.use(metricsMiddleware);
 
 const db = require("./app/models");
 db.mongoose
@@ -53,41 +37,6 @@ db.mongoose
 //  res.json({ message: "Welcome to CIT application." });
 //});
 
-
-// Create counters for total and failed API calls
-const totalApiCallsCounter = new prometheus.Counter({
-  name: 'my_app_total_api_calls',
-  help: 'Total number of API calls',
-});
-const failedApiCallsCounter = new prometheus.Counter({
-  name: 'my_app_failed_api_calls',
-  help: 'Number of failed API calls',
-});
-
-// Middleware to increment total and failed API call counters
-app.use((req, res, next) => {
-  totalApiCallsCounter.inc();
-
-  res.on('finish', () => {
-    if (res.statusCode >= 400) {
-      failedApiCallsCounter.inc();
-    }
-  });
-
-  next();
-});
-
-// Expose metrics
-app.get('/metrics', (req, res) => {
-  res.set('Content-Type', prometheus.register.contentType);
-  prometheus.register.metrics().then(metrics => {
-    res.end(metrics.toString()); // <-- convert to string
-  }).catch(err => {
-    console.error(err);
-    res.status(500).end();
-  });
-});
-
 require("./app/routes/incident.routes")(app);
 require("./app/routes/emp.routes")(app);
 
@@ -97,4 +46,5 @@ const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
 
